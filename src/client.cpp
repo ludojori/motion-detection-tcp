e6433a5dd4ce55ae2378b3092228b5feb7dce521
ludojori <jordanovden@abv.bg>
2022-03-03 18:13:37 +0000

    Initial commit

--- src/client.cpp
+++ src/client.cpp
@@ -1,12 +1,13 @@
-//#include <netdb.h>
-//#include <netinet/in.h>
 #include <iostream>
-#include <socketapi.h>
-#include <winsock.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "protocol.h"
 
 #define SENSITIVITY_LENGTH 8
 
-bool send_sensitivity(int socketID, uint64_t sensitivity)
+bool try_send_sensitivity(int socketID, uint64_t sensitivity)
 {
     char buffer[SENSITIVITY_LENGTH];
     for (int i = SENSITIVITY_LENGTH - 1; i >= 0; i--)
@@ -15,12 +16,78 @@ bool send_sensitivity(int socketID, uint64_t sensitivity)
     }
 
     int sending = send(socketID, buffer, SENSITIVITY_LENGTH, 0);
-    if (sending == ERROR)
+    if (sending < 0)
     {
         return false;
     }
 
     return true;
+}
+
+bool try_receive_notification(int socketID)
+{
+	int bytesReceived = 0;
+	ServerNotification notification;
+
+	int statusSize = sizeof(notification.status);
+	char statusByte;
+	bytesReceived = recv(socketID, &statusByte, statusSize, 0);
+	
+	if (bytesReceived < statusSize)
+	{
+		std::cerr << "Failed to receive status." << std::endl;
+		return false;
+	}
+	
+	notification.status = (Status)statusByte;
+	
+	if (notification.status == Status::STILL_IMAGE)
+	{
+		std::cout << "Warning: status received is STILL_IMAGE." << std::endl;
+	}
+
+	int dimensionSize = sizeof(notification.width);
+
+	if (dimensionSize != sizeof(notification.height))
+	{
+		std::cout << "Warning: size of width and height do not match." << std::endl;
+	}
+	
+	char dimensionBuffer[dimensionSize];
+	bytesReceived = recv(socketID, &dimensionBuffer, dimensionSize, 0);
+
+	if (bytesReceived < dimensionSize)
+	{
+		std::cerr << "Failed to receive width." << std::endl;
+		return false;
+	}
+
+	int width;
+	// TODO: assign width
+
+	bytesReceived = recv(socketID, &dimensionBuffer, dimensionSize, 0);
+	if (bytesReceived < dimensionSize)
+	{
+		std::cerr << "Failed to receive height." << std::endl;
+		return false;
+	}
+
+	int height;
+	// TODO: assign height
+
+	int imageSize = width * height * sizeof(uint32_t);
+	char* imageBuffer = new char[imageSize];
+	bytesReceived = recv(socketID, imageBuffer, imageSize, 0);
+
+	if (bytesReceived < imageSize)
+	{
+		std::cerr << "Failed to receive image." << std::endl;
+		return false;
+	}
+
+	notification.image = imageBuffer;
+
+	return true;
 }
 
 void save_to_png(uint32_t width, uint32_t height, char* bytes)
@@ -30,47 +97,50 @@ void save_to_png(uint32_t width, uint32_t height, char* bytes)
 
 int main(int argc, char** args)
 {
-    if (argc < 4)
+    if (argc != 4)
     {
         std::cerr << "Invalid number of parameters." << std::endl;
-        return 1;
+        return (int)Error::PARAMCOUNT;
     }
 
     hostent* host = gethostbyname(args[1]);
 
     int socketID = socket(AF_INET, SOCK_STREAM, 0);
-    if (socketID == SOCKET_ERROR)
+    if (socketID < 0)
     {
         std::cerr << "Failed to create socket." << std::endl;
-        return 1;
+        return (int)Error::SOCKET;
     }
 
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons((u_short)strtoul(args[2], NULL, 0));
-    std::memcpy(&serverAddress.sin_addr.s_addr, host->h_addr, host->h_length);
+    memcpy(&serverAddress.sin_addr.s_addr, host->h_addr, host->h_length);
 
     int connection = connect(socketID, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-    if (connection == ERROR_NOT_CONNECTED)
+    if (connection < 0)
     {
         std::cerr << "Failed to connect to server." << std::endl;
-        return 1;
+        return (int)Error::CONNECTION;
     }
 
     uint64_t sensitivity = atoi(args[3]);
-    if (!send_sensitivity(socketID, sensitivity))
+    if (!try_send_sensitivity(socketID, sensitivity))
     {
         std::cerr << "Failed to send sensitivity threshold." << std::endl;
-        return 1;
+        return (int)Error::SEND;
     }
-
-    char buffer[1000000000];
+    
     while (true)
     {
-        int bytesReceived = recv(socketID, buffer, 10000, 0);
+        if (!try_receive_notification(socketID))
+        {
+        	std::cerr << "An error occured while receiving data." << std::endl;
+        	return (int)Error::RECEIVE;
+        }
     }
 
-    closesocket(socketID);
+    close(socketID);
 
     return 0;
 }
--- src/protocol.h
+++ src/protocol.h
@@ -1,10 +1,25 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
-#include <cstdint>
+#include <stdint.h>
+
+#define MAX_IMAGE_RESOLUTION 1000000;
+
+enum class Error : int
+{
+	NONE,
+	PARAMCOUNT,
+	PARAMFORMAT,
+	SOCKET,
+	BIND,
+	CONNECTION,
+	SEND,
+	RECEIVE
+};
 
 enum class Status : char
 {
-	STILL_IMAGE, MOTION_DETECTED
+	STILL_IMAGE = 0,
+	MOTION_DETECTED = 1
 };
 
 struct ServerNotification
@@ -12,7 +27,7 @@ struct ServerNotification
 	Status status;
 	uint32_t width;
 	uint32_t height;
-	char image[1000000];
+	char* image;
 };
 
 #endif
